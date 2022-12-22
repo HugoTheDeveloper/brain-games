0ad5b77c1bb714348c709e1e21aa5ab7cecb81b8
bogdan <telioko@tutanota.com>
2022-12-22 21:36:40 +0300

    update .gitignore, rename brain_games.py, add cli.py, add new interactive greeting

--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
 
 def main():
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ =='__main__':
     main()
