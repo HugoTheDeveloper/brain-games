e14aeff46f1284078fad77ca6d2206d679c02899
bogdan <telioko@tutanota.com>
2023-02-21 16:36:47 +0300

    update brain_even.py

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -6,12 +6,12 @@ RULE = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def is_even_num(num):
     if num % 2 == 0:
-        return 'yes'
+        return True
     else:
-        return 'no'
+        return False
 
 
 def generate_question_n_answer():
     random_num = randint(1, 100)
-    correct_answer = is_even_num(random_num)
+    correct_answer = 'yes' if is_even_num(random_num) else 'no'
     return random_num, correct_answer
